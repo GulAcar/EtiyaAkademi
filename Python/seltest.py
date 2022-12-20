bc4cd124c7b61ddb881b8a9a494a0d676f4b111f
gulacar <76070091+GulAcar@users.noreply.github.com>
2022-12-20 23:40:07 +0300

    ....

--- Python/seltest.py
+++ Python/seltest.py
@@ -4,8 +4,19 @@
 #webdriverı kuramadığımız için exe dosyasını ekledik
 
 from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
 driver=webdriver.Chrome()
-driver.get("https://www.google.com/crome/browser")
+driver.get("https://www.google.com/")
+sleep(2)
 
 #html locaters
-driver.find_element()
+
+driver.find_element(By.NAME,"q")
+#bunu bir degere atayabiliriz
+input= driver.find_element(By.NAME,"q")
+input.send_keys("Deneme")
+sleep(3)
+
+serachBtn =driver.find_element(By.NAME,"btnK")
+serachBtn.click()
